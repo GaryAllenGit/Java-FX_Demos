7953fd867fa56b0cf8ea4ae7bc0b4bbffb5f86d8
Gary Allen <g.allen@hud.ac.uk>
2018-05-01 15:46:06 +0100

    Added a demo to show data in a table being displayed in a corresponding pie chart

---
+++ FX_TableAndPieChart_NO_fxml/src/fx_table_and_pie_chart/TableAndPieChartDemo.java
@@ -0,0 +1,61 @@
+package fx_table_and_pie_chart;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+
+public class TableAndPieChartDemo extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        ObservableList<PieChart.Data> emailData =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Monday", 75),
+                        new PieChart.Data("Tuesday", 58),
+                        new PieChart.Data("Wednesday", 52),
+                        new PieChart.Data("Thursday", 59),
+                        new PieChart.Data("Friday", 58),
+                        new PieChart.Data("Saturday", 15),
+                        new PieChart.Data("Sunday", 10));
+
+        PieChart chart = new PieChart(emailData);
+        chart.setTitle("Daily Emails Received");
+
+        TableView tv = new TableView<>(emailData);
+        tv.setEditable(true);
+
+        TableColumn column1  = new TableColumn("Day");
+        column1.setPrefWidth(120);
+        column1.setCellValueFactory(new PropertyValueFactory("name"));
+        column1.setCellFactory(TextFieldTableCell.forTableColumn());
+        column1.setEditable(false);
+
+        TableColumn column2 = new TableColumn("No. of Emails");
+        column2.setPrefWidth(120);
+        column2.setCellValueFactory(new PropertyValueFactory("pieValue"));
+        column2.setCellFactory(TextFieldTableCell.forTableColumn(new NumberStringConverter()));
+
+        tv.getColumns().addAll(column1,column2);
+
+        Scene scene = new Scene(new HBox(tv, chart));
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
+
--- README.md
+++ README.md
@@ -5,12 +5,14 @@ Dr Gary Allen, The University of Huddersfield
 A number of Java FX Demo projects for use by students.
 
 Currently there is:
+
 	a classic HelloWorld created without an fxml file,
 	a set of pop-up window demos of varying complexity (created without fxml files),
 	a HelloWorld created with scene builder and xfml,
 	a CountingButton created with scene builder and xfml,
 	a (pretty poor) Calculator created with scene builder and xfml,
 	a Collections demo using the ListView and ObservableList classes, created with scene builder and xfml.
+	a Table and Pie Chart demo heavily based upon one found online at https://stackoverflow.com/questions/23964885/binding-chart-to-tableview-javafx
 
 These may all be updated over time, and other demos may be added.
 
